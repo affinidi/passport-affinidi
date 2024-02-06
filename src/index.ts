70ec48519f37540684c1853147c08a321f69c649
Paramesh Kamarthi <kamarthiparamesh@gmail.com>
2024-02-06 12:54:39 +0530

    fix: remove cookie same-site

--- src/index.ts
+++ src/index.ts
@@ -56,7 +56,6 @@ export const affinidiProvider = async (app: any, options: ProviderOptionsType) =
       saveUninitialized: true,
       cookie: {
         secure: process.env.NODE_ENV === 'production',
-        sameSite: 'none',
         maxAge: 1000 * 60 * 60 * 24 * 1, // 1 day
       },
       unset: 'destroy',
