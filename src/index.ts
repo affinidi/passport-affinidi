afc2f2e9950f37776f255a59d6f770f84756be4b
Paramesh Kamarthi <kamarthiparamesh@gmail.com>
2024-02-06 12:39:19 +0530

    fix: updated options

--- src/index.ts
+++ src/index.ts
@@ -50,16 +50,17 @@ export const affinidiProvider = async (app: any, options: ProviderOptionsType) =
   passport.use(options.id, strategy)
 
   app.use(
-    expressSesssion(
-      options.expressSesssion || {
+    expressSesssion({
         secret: options.id,
         resave: false,
         saveUninitialized: true,
         cookie: {
           secure: process.env.NODE_ENV === 'production',
+          sameSite: "none",
           maxAge: 1000 * 60 * 60 * 24 * 1, // 1 day
         },
         unset: 'destroy',
+        ...options.expressSesssion
       },
     ),
   )
